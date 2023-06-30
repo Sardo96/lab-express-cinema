1592d8427f8d964dd86d7f7205c2c5f5ccc749bc
Miguel Sardo <miguel.sardoms@gmail.com>
2023-06-30 22:34:44 +0100

    done

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,29 @@
 const express = require('express');
 const router = express.Router();
+const Movie = require('../models/Movie.model');
+
 
 /* GET home page */
 router.get('/', (req, res, next) => res.render('index'));
+
+router.get("/movies", async (req, res, next) => {
+    try {
+      const moviesFromDB = await Movie.find();
+      console.log(moviesFromDB);
+      res.render("movies", { movies: moviesFromDB });
+    } catch (error) {
+      next(error);
+    }
+  });
+  
+  router.get("/movies/:id", async (req, res, next) => {
+    try {
+      const movieId = req.params.id;
+      const movieFromDB = await Movie.findById(movieId);
+      res.render("movie-details", { movie: movieFromDB });
+    } catch (error) {
+      next(error);
+    }
+  });
 
 module.exports = router;
